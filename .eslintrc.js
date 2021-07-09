e95083a1d2f3f98d8614e7bd179b2989100718f5
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-07-09 15:37:01 +0200

    chore: config lint rule `explicit-module-boundary-types`
    As mentioned on the page of the rule:
    https://github.com/typescript-eslint/typescript-eslint/blob/v4.28.2/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md
    
    Inspired from:
    https://github.com/peopledoc/synapps-hr-admin-front/pull/178/commits/0d98dcc74400060223f16f1389019659e4ff9e3d

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,9 @@ module.exports = {
   rules: {
     // --- TypeScript rules
 
+    // This rule is overriden and configured in the `overrides` Array below
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
     'no-empty-function': 'off',
     '@typescript-eslint/no-empty-function': ['error'],
   },
@@ -58,6 +61,16 @@ module.exports = {
       },
       plugins: ['node'],
       extends: ['plugin:node/recommended'],
+    },
+
+    // TypeScript related
+
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': ['error'],
+      },
     },
   ],
 };
